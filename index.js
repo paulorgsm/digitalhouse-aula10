20ff5c3c404a31241eac60eb876ad326aeb6258f
Paulo Moreira <pauloregisml@gmail.com>
2021-03-03 20:42:25 -0300

    Adicionando Hello World

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Servidor Rodando OK na porta 3000')
-});
 //há 65536 portas, é importante estar disponível
 //ver todas as portas sendo usadas netstat -o -n -a
 //necessário dar CRTL + C para sair, pois fica infinitando tentando...
@@ -12,3 +9,11 @@ app.listen(3000, () => {
 //package-lock.json/ verifica todas as dependências - útil mais para devOps para compilar com cliente
 //git restore ou git reset limpa o que foi adicionado
 //git rm --cached <file>...
+
+app.get('/', (req, res)=>{
+    return res.send('Hello World'); //return é boa prática
+});
+
+app.listen(3000, () => {
+    console.log('Servidor Rodando OK na porta 3000');
+});
